13448643bdaa2e5f8af671093b5f960f0d2b0204
n3pite00 <n3pite00@students.oamk.fi>
2024-10-16 18:32:08 +0300

    Lisätty sivustolle.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import Header from './components/Header.jsx'
 import LogoImg from './assets/eco-5465429_640.png'
 import Products from './components/Dropdown.jsx'
-import Quantity from './components/Quantity.jsx'
-import ProductionInfo from './components/info.jsx'
 
 
 function App() {
