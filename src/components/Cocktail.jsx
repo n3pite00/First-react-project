585692ad5550c98bfe19ed5931b798f7e567e498
n3pite00 <n3pite00@students.oamk.fi>
2024-10-17 21:33:00 +0300

    Merged site finish

---
+++ src/components/Cocktail.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import Axios from "axios"
+import "../styles/cocktailStyle.css"
+
+function Cocktail() {
+
+    const [info, setInfo] = useState("")
+
+    function generate() {
+
+    Axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php').then(res => {
+        setInfo(res.data.drinks[0])
+        
+    })
+    }
+    return (
+    <div className="container">
+        <h1> Generate a Random Cocktail!</h1>
+        <button className="Cocktailbutton" onClick = {generate}> Cocktail! </button>
+        <h2>{info.strDrink}</h2>
+        <p>{info.strInstructions}</p>
+    </div>
+    )
+}
+
+export default Cocktail
